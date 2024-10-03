2a548f87b0c45c720bfb5e517b8e6a15128b80e1
Huyyun_Hyun <96609566+Huyyun@users.noreply.github.com>
2024-10-03 17:47:51 +0700

    Ex7_Session2

--- Module 01/Ex7_ss2.js
+++ Module 01/Ex7_ss2.js
@@ -2,17 +2,14 @@ let n = parseInt(prompt("Nhập vào một số nguyên dương: "));
 let reverse = 0;
 let original = n; 
 
-while (true) {
-    if (n > 0) { 
+if (n <= 0 || isNaN(n)) {
+    console.log("Vui lòng nhập một số nguyên dương hợp lệ.");
+} else {
+    while (n > 0) { 
         let last = n % 10; 
         reverse = reverse * 10 + last;  
         n = Math.floor(n / 10);  
     }
-    if (original < 0){
-        console.log("Số ban đầu: " + original);
-        console.log("Số đảo ngược là: " + reverse);
-        break;
-    } else {
-        console.log("Vui lòng nhập một số nguyên dương hợp lệ.");
-    }
+    console.log("Số ban đầu: " + original);
+    console.log("Số đảo ngược là: " + reverse);
 }
