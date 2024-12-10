590d7805e12afc3108273f96ab9ae81acc1b6b52
Huyyun_Hyun <96609566+Huyyun@users.noreply.github.com>
2024-12-10 16:49:49 +0700

    Session02

--- Session 02 - TS/dist/Ex4_ss2.js
+++ Session 02 - TS/dist/Ex4_ss2.js
@@ -1,1 +1,34 @@
 "use strict";
+function bubbleSortDescending(arr) {
+    for (let i = 0; i <= arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+function main() {
+    let numbers = [];
+    console.log(`Nhập vào 10 số nguyên:`);
+    for (let i = 0; i < 10; i++) {
+        let input = prompt(`Nhập số thứ ${i + 1}:`);
+        if (input !== null) {
+            let num = parseInt(input, 10);
+            if (!isNaN(num)) {
+                numbers[i] = num;
+            }
+            else {
+                console.error("Giá trị không hợp lệ, vui lòng nhập số nguyên!");
+                i--;
+            }
+        }
+    }
+    console.log(`Mảng đã nhập: [${numbers.join(", ")}]`);
+    const sortedArray = bubbleSortDescending(numbers);
+    console.log(`Mảng sau khi sắp xếp theo thứ tự giảm dần: [${sortedArray.join(", ")}]`);
+}
+main();
