f2354e4b871eec5caa73712f0f59c1400d71da0b
Huyyun_Hyun <96609566+Huyyun@users.noreply.github.com>
2024-12-10 17:01:28 +0700

    Session02

---
+++ Session 02 - TS/dist/Ex5_ss2.js
@@ -0,0 +1,10 @@
+"use strict";
+let array2D = [
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27]
+];
+console.log("Mảng 2 chiều:");
+for (let i = 0; i < array2D.length; i++) {
+    console.log(array2D[i].join(", "));
+}
---
+++ Session 02 - TS/src/Ex5_ss2.ts
@@ -0,0 +1,11 @@
+// Khởi tạo mảng 2 chiều
+let array2D: number[][] = [
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27]
+];
+
+console.log("Mảng 2 chiều:");
+for (let i = 0; i < array2D.length; i++) {
+    console.log(array2D[i].join(", "));
+}
